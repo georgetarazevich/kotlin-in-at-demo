948a83315d778211fb7cf956f00f9a2af466f2dc
ASAntonov <asantonov@it-one.ru>
2024-05-02 22:34:52 +0300

    2. Добавление логирования в проект на Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,23 @@ plugins {
 group = "org.itonemeetup"
 version = "1.0-SNAPSHOT"
 
+/* Версии зависимостей */
 val jvmToolChain = 17
+val log4jApiKotlinVersion = "1.4.0"
+val log4jVersion = "2.23.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    /* Log4j Kotlin API: https://logging.apache.org/log4j/kotlin/latest/ */
+    implementation("org.apache.logging.log4j:log4j-api-kotlin:$log4jApiKotlinVersion")
+    /* Импорт BOM: https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import */
+    implementation(platform("org.apache.logging.log4j:log4j-bom:$log4jVersion"))
+    /* При импорте BOM версию артефакта можно не указывать */
+    implementation("org.apache.logging.log4j:log4j-core")
+
     testImplementation(kotlin("test"))
 }
 
--- src/test/kotlin/basic/BasicTests.kt
+++ src/test/kotlin/basic/BasicTests.kt
@@ -1,6 +1,7 @@
 package basic
 
 import java.lang.Integer.sum
+import org.apache.logging.log4j.kotlin.logger
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -13,5 +14,18 @@ class BasicTests {
         val a = 1
         val b = 41
         assertEquals(42, sum(a, b), "Неверный результат sum($a, $b)!")
+    }
+
+    @Test
+    fun `test KotlinLogging`() {
+        /* Получение дополнительных данных */
+        val createLogMessage: () -> String = {
+            println("Получение данных для лога...")
+            "Вычисляемое сообщение!"
+        }
+        /* Логирование */
+        logger.trace("Сообщение в режиме трассировки! ${createLogMessage()}")
+        logger.trace { "Еще одно сообщение в режиме трассировки! ${createLogMessage()}" }
+        logger.info { "Информационное сообщение!" }
     }
 }
